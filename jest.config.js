604ee145ffe2b668d58622eeaabbe938ab640fd7
Sébastien David <sdavid@alkemics.com>
2020-04-08 11:59:32 +0200

    promise polyfill for node < v12

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,9 @@
 module.exports = {
   testMatch: ['**/*.test.js'],
-  setupFiles: ['<rootDir>/node_modules/regenerator-runtime/runtime'],
+  setupFiles: [
+    '<rootDir>/node_modules/regenerator-runtime/runtime',
+    '<rootDir>/node_modules/core-js/features/promise/index',
+  ],
   transform: {
     '^.+\\.js$': 'babel-jest',
   },
