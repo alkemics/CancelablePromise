6413c46c354d52249a678bd64d2dbc031db5a13b
Sébastien David (seb) <50570955+alk-sdavid@users.noreply.github.com>
2022-04-27 14:18:47 +0200

    fix(tests): fix tests for jest v28

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,5 @@
 module.exports = {
   testMatch: ['**/*.test.ts'],
-  setupFiles: [
-    '<rootDir>/node_modules/regenerator-runtime/runtime',
-    '<rootDir>/node_modules/core-js/features/promise/index',
-  ],
   transform: {
     '^.+\\.(j|t)s$': 'babel-jest',
   },
