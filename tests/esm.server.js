1808116709bb88ca9cea98c0a3bf7e95b8e8df17
Sébastien David (seb) <50570955+alk-sdavid@users.noreply.github.com>
2022-12-07 11:31:39 +0100

    fix(e2e): rewrite e2e tests for cypress v12

--- tests/esm.server.js
+++ tests/esm.server.js
@@ -13,6 +13,7 @@ app.get('/ok', (req, res) => {
   res.send('ok');
 });
 app.use('/esm', express.static(path.join(__dirname, '../esm'), staticOptions));
+app.use('/umd', express.static(path.join(__dirname, '../umd'), staticOptions));
 app.use('/', express.static(__dirname, staticOptions));
 
 app.listen(port, () => {
--- tests/esm.spec.js
+++ tests/esm.spec.js
@@ -1,20 +1,9 @@
 import { expectCancelablePromise } from './cancelable-promise.expect';
 
 describe('ESM cancelable promise', () => {
-  let cancelable;
-  let CancelablePromise;
-
-  it('should be loaded', () => {
-    cy.intercept('/ok').as('ok');
-    cy.visit('http://localhost:3000/esm.html');
-    cy.wait('@ok');
-    cy.window().its('CancelablePromise').should('be.ok');
-    cy.window().its('cancelable').should('be.ok');
-    cy.window().then((win) => {
-      cancelable = win.cancelable;
-      CancelablePromise = win.CancelablePromise;
-    });
+  expectCancelablePromise({
+    html: 'esm',
+    cancelablePath: 'cancelable',
+    CancelablePromisePath: 'CancelablePromise',
   });
-
-  expectCancelablePromise(() => ({ cancelable, CancelablePromise }));
 });
